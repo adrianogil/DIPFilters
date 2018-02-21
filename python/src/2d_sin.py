21160bf8d83a324efb75247c9dc8da168c5af789
Adriano Gil <adrianogil.san@gmail.com>
2018-02-21 19:03:14 -0400

    WIP ploting 2D waves

---
+++ python/src/2d_sin.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+height = 128
+width = 128
+
+def cos2d(x,y):
+    wx = np.pi / 100;
+    # wy = np.pi / 10;
+    wy = 0 / 10;
+    return int(100 * (0.5 + np.cos(wx * x + wy * y)/2))
+
+img = np.zeros((height, width), np.uint8)
+
+for y in xrange(0, height):
+    for x in xrange(0, width):
+        img[y,x] = cos2d(x,y)
+
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
+
+# plt.imshow(img, cmap = 'gray')
+# plt.show()
