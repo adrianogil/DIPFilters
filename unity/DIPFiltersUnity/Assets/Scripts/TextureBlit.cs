b6c4a24c45a99028a1bdd41a37a067490d762083
Adriano Gil <adrianogil.san@gmail.com>
2018-02-21 16:03:50 -0400

    Blit in texture

---
+++ unity/DIPFiltersUnity/Assets/Scripts/TextureBlit.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TextureBlit : MonoBehaviour {
+
+    public RenderTexture texture;
+    public Material blitMaterial;
+
+    private RenderTexture temp = null;
+
+    [ContextMenu("Load RTX from Material")]
+    public void LoadRTxFromMaterial()
+    {
+        Graphics.Blit(GetComponent<MeshRenderer>().sharedMaterial.mainTexture, texture);
+    }
+
+	// Use this for initialization
+	void CreateTempRTx () {
+        temp = new RenderTexture(texture.width, texture.height, 24);
+	}
+
+    public void Blit()
+    {
+        if (temp == null) {
+            CreateTempRTx();
+        }
+        // Graphics.Blit(texture, texture, blitMaterial) // Only on Mac?
+        Graphics.Blit(texture, temp, blitMaterial);
+        Graphics.Blit(temp, texture);
+    }
+}
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(TextureBlit))]
+public class TextureBlitEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        TextureBlit editorObj = target as TextureBlit;
+
+        if (editorObj == null) return;
+
+        if (GUILayout.Button("Blit"))
+        {
+            editorObj.Blit();
+        }
+    }
+
+}
+#endif
